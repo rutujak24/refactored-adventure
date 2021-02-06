3d6514b97a378c8f00686a17be50b99d56970b71
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2021-02-06 09:16:49 +0530

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -27,22 +27,36 @@ Expected Auxiliary Space: O(N)
 
 Constraints: 1 ≤ |S| ≤ 105 
 
-class Solution { //JAVA TLE
-public String compress(String s) {
-int i;
-String res= "";
-i = s.length()/2 - 1;
-while(i >= 0){
-if(s.substring(0, i+1).equals(s.substring(i+1, 2*i+2))){
-res = s.substring(0, i+1) + "*" + s.substring(2*i+2 , s.length());
-s = res;
-i = s.length()/2 -1;
-}
-else
-i--;
-}
-return res;
-}
-}
 */
 
+string compress(string s)
+    {
+        // Your code goes here
+        int n = s.size();
+        vector<int> v(n);
+        for(int i = 1; i<n;i++){
+            int j = v[i-1];
+            while(j>0 && s[i]!=s[j]){
+                j = v[j-1];
+                
+            }
+            if(s[i]==s[j]) j++;
+            v[i] = j;
+        }
+        
+        string ans;
+        for(int i=n-1;i>=0;i--){
+            if(i%2){
+                if(v[i]>=(i+1)/2 && (i+1)%(2 * (i+1-v[i])) == 0){
+                    ans.push_back('*');
+                    i/=2;i++;
+                }
+                else {ans.push_back(s[i]);}
+                
+            }
+            else {ans.push_back(s[i]);}
+        }
+        reverse(ans.begin(), ans.end());
+       
+        return ans;
+    }
