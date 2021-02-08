c57419ccad43dc91db3cc5d6f477584a2555d975
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2021-02-08 12:08:07 +0530

    Create 13.cpp

---
+++ 13.cpp
@@ -0,0 +1,2 @@
+/* Check if the given preorder, postorder and inorder traversals are of same tree or not
+https://www.geeksforgeeks.org/check-if-given-preorder-inorder-and-postorder-traversals-are-of-same-tree-set-2/ */
